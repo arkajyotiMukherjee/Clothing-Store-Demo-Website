7db5ac41cb35ec367bf33ad87e452c56569fc709
arko-loves-to-code <arkajyoti31@gmail.com>
2018-02-09 23:21:02 +0530

    Initial Commit

---
+++ js/functions.js
@@ -0,0 +1,53 @@
+$(window).scroll(function(){
+  var wScroll = $(this).scrollTop()
+  
+  //top parallax
+  
+  $('.logo').css({
+      'transform' : 'translate(0px, ' + wScroll/2 + '%)'
+  })
+  
+  $('.back-bird').css({
+      'transform' : 'translate(0px, '+ wScroll/4 + '%)'
+  })
+
+  $('.fore-bird').css({
+      'transform' : 'translate(0px, -'+ wScroll/40 + '%)'
+  })
+
+  if(wScroll > $('.clothes-pics').offset().top - ($(window).height() / 1.2)){
+
+    $('.clothes-pics figure').each(function (i) {
+
+        setTimeout(function () {
+            $('.clothes-pics figure').eq(i).addClass('is-showing')
+        }, (700 * Math.exp(i*0.14) - 700))   
+    })
+  }
+
+  //periscope
+
+  if(wScroll > $('.large-window').offset().top - $(window).height()){
+
+    var opacity = (wScroll - $('.large-window').offset().top + 400) / (wScroll/5);
+  
+    $('.large-window').css({'background-position' : 'center '+ (wScroll - $('.large-window').offset().top) +'px'})
+
+    $('.window-tint').css({'opacity': opacity})
+  }
+
+
+  //Blog posts
+
+  if(wScroll > $('.blog-posts').offset().top - $(window).height()){
+
+    var offset = Math.min(0, wScroll - $('.blog-posts').offset().top + $(window).height() - 400)
+    
+    $('.post-1').css({'transform': 'translate('+offset+'px,' +Math.abs(offset * 0.3)+'px)'})
+
+    $('.post-3').css({'transform': 'translate('+ Math.abs(offset)+'px,' +Math.abs(offset * 0.3)+'px)'})
+  }
+
+})
+
+
